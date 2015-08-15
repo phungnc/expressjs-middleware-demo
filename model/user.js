1af6d73c8ea6394e6da1204397e3a0783014cc97
phungnc <phungnc@gmail.com>
2015-08-15 08:50:38 +0700

    - edit callback predicate as Array.find spec - return result from Array.find method - check return user is undefined instead of false

--- model/user.js
+++ model/user.js
@@ -15,9 +15,8 @@ var userData = [
   }
 ];
 
-exports.findUser = function findUser(name) {
-  userData.find(function(user) {
-    console.log(user.name);
-    if (user.name === name) return user; else return false;
+exports.findUser = function(name) {
+  return userData.find(function (user) {
+    return user.name == name;
   });
-};
+}
